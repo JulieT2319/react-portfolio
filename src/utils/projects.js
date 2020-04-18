7200f8c74e7b0d434ef6d6aeacf560a7fb253ae1
Julie T <joules2319@gmail.com>
2020-04-18 15:13:16 -0600

    created project object

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,3 +1,44 @@
 export var projects = [
-
+  {
+    title: "ASCII Art Generator",
+    description: "Search an image and convert it to text art.",
+    screenshot: "/images/ascii-art-ss.png",
+    repo: "https://github.com/Brendan-Hembury/Project-1",
+    deployment: "https://brendan-hembury.github.io/Project-1/"
+  },
+  {
+    title: "Weather Forecast",
+    description: "Search for the current weather and 5-day-forecast of any city.",
+    screenshot: "/images/weather-app-ss.png",
+    repo: "https://github.com/JulieT2319/weather-app",
+    deployment: "https://juliet2319.github.io/weather-app/"
+  },
+  {
+    title: "Daily Schedule",
+    description: "Keep track of a daily schedule",
+    screenshot: "/images/day-planner-ss.png",
+    repo: "https://github.com/JulieT2319/day-planner",
+    deployment: "https://juliet2319.github.io/day-planner/"
+  },
+  {
+    title: "Oscar Watch",
+    description: "Search through the database of Oscar nominated films to create watchlists.",
+    screenshot: "/oscar-watch-ss.jpg",
+    repo: "https://github.com/Ericcwong/Oscar-Watch",
+    deployment: "https://project2-oscar-watch.herokuapp.com/"
+  },
+  {
+    title: "Burger Tracker",
+    description: "Add burgers to the database and track if they've been eaten.",
+    screenshot: "/images/burger-ss.jpg",
+    repo: "https://github.com/JulieT2319/burger",
+    deployment: "https://evening-island-97795.herokuapp.com/"
+  },
+  {
+    title: "Company Database",
+    description: "A command line application to track your company employees.",
+    screenshot: "/images/department-table-ss.jpg",
+    repo: "https://github.com/JulieT2319/employee-tracking",
+    deployment: "https://github.com/JulieT2319/employee-tracking.git"
+  }
 ];
