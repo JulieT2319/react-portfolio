7bbab2d7ddcfe0ac8e2480d361ecd701fc92fcb1
Julie T <joules2319@gmail.com>
2020-04-18 14:16:05 -0600

    initial file set up

---
+++ src/pages/Main.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Card from "../components/Card"
+import SearchContainer from '../components/SearchContainer'
+import { employees } from '../utils/projects'
+
+export default class Main extends Component {
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className="center container">
+
+        <Card
+        />
+
+      </div>
+    )
+  }
+}
+
