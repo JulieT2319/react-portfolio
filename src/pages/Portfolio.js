1872134ae81e6f19f79c9c1e1122ba0b21571caf
Julie T <joules2319@gmail.com>
2020-04-18 18:29:13 -0600

    adjusted portfolio divs

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,19 +6,21 @@ import { projects } from '../utils/projects';
 export default function Portfolio() {
 
   return (
-    <div class="row" >
-      {
-        projects.map(project => (
-          <Card
-            title={project.title}
-            description={project.description}
-            screenshot={project.screenshot}
-            repo={project.repo}
-            deployment={project.deployment}
-          />
-        )
-        )
-      }
-    </div >
+    <div class="container ">
+      <div class="row" >
+        {
+          projects.map(project => (
+            <Card
+              title={project.title}
+              description={project.description}
+              screenshot={project.screenshot}
+              repo={project.repo}
+              deployment={project.deployment}
+            />
+          )
+          )
+        }
+      </div >
+    </div>
   )
 }
