8a5e0e36757771498e86a14b6a5dc98434fa5ce8
Julie T <joules2319@gmail.com>
2020-04-18 18:25:20 -0600

    Updates

---
+++ precache-manifest.ece373b62c84c60ba1deda253f4e92fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f5436a2faf731e13652a13b41bc0e9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8ba57ff5ea8f33990ddc",
+    "url": "/react-portfolio/static/css/main.eec7fab0.chunk.css"
+  },
+  {
+    "revision": "98084e266715ddeb406b",
+    "url": "/react-portfolio/static/js/2.e5c8ce99.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/react-portfolio/static/js/2.e5c8ce99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba57ff5ea8f33990ddc",
+    "url": "/react-portfolio/static/js/main.4b5676fc.chunk.js"
+  },
+  {
+    "revision": "bf60400728853ad49156",
+    "url": "/react-portfolio/static/js/runtime-main.453a5be9.js"
+  },
+  {
+    "revision": "100cb48a020cb7ae8026c05204e7090f",
+    "url": "/react-portfolio/static/media/Kingthings_Xstitch-webfont.100cb48a.ttf"
+  },
+  {
+    "revision": "14f0a37d8f4071674730c6f556cc22df",
+    "url": "/react-portfolio/static/media/Kingthings_Xstitch-webfont.14f0a37d.eot"
+  },
+  {
+    "revision": "759d5107d87bc8ffa9f6416a2445430a",
+    "url": "/react-portfolio/static/media/Kingthings_Xstitch-webfont.759d5107.woff"
+  },
+  {
+    "revision": "c418d0dee3e19315d81d2c4b2444800a",
+    "url": "/react-portfolio/static/media/Kingthings_Xstitch-webfont.c418d0de.svg"
+  },
+  {
+    "revision": "5a8123e594e972ac4ba682b9b83907a8",
+    "url": "/react-portfolio/static/media/aidabg.5a8123e5.jpg"
+  },
+  {
+    "revision": "683da37537c153310e7ff55ec53fff7a",
+    "url": "/react-portfolio/static/media/julie.683da375.jpg"
+  }
+]);
